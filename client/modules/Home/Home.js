e70c00bc16f1c4fda3700af5c6a5b226fca39503
Tomasz Sokalski <tomm1slaw@o2.pl>
2018-11-13 20:40:31 +0100

    Add home module

---
+++ client/modules/Home/Home.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+ // Import Style
+// import styles from './Home.css';
+ class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Witaj na stronie startowej!</h2>
+      </div>
+    );
+  }
+}
+ const mapStateToProps = (state) => {
+  return {};
+};
+ const mapDispatchToProps = (dispatch) => {
+  return {};
+};
+ Home.propTypes = {
+};
+ export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Home);
